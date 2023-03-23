19869b376f23da6c53f60c477b552e5a5e347b4b
Gabriel Souza <rorato91@gmail.com>
2023-03-23 09:53:36 -0300

    treinando com exer
    exer 04 não entendi muito bem

---
+++ CursoCsharpUdemy/exer/outros/Exer05.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exer.outros
+{
+    public class Exer05
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
