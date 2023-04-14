f187036c48b48d007d897826bdb0f71157521a18
Gabriel Souza <rorato91@gmail.com>
2023-04-14 17:18:49 -0300

    Aulas 02 até 05

---
+++ CursoPython3/section01/aula02.py
@@ -0,0 +1,5 @@
+# \r\n - CRLF
+# \n - LF
+print(12, 34, sep="-", end="\n##\n")
+print(56, 78, sep="-", end="\n")
+print(56, 78, sep="-", end="\n")
---
+++ CursoPython3/section01/aula03.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Gabriel Souza')
+print(1, 'Gabriel "Souza"')
+
+# Aspas duplas
+print("Gabriel Souza")
+print(2, "Gabriel 'Souza'")
+
+# Escape
+print("Gabriel \"Souza\"")
+
+# r
+print(r"Gabriel \"Souza\"")
