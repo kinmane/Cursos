5f4ea000728da7c64a87600d8ecc12756c63a275
Gabriel Souza <rorato91@gmail.com>
2023-04-17 13:42:21 -0300

    aula 06 até 10

---
+++ CursoPython3/section01/aula08.py
@@ -0,0 +1,13 @@
+nome = 'Gabriel'
+sobrenome = 'Souza'
+idade = 25
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+
+print('Nome: ', nome)
+print('Nome: ', sobrenome)
+print('Nome: ', idade)
+print('Nome: ', ano_nascimento)
+print('Nome: ', maior_de_idade)
+print('Nome: ', altura_metros)
