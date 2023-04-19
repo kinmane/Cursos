58abb8ebd361ada7ae3ff6476a7ce0cac6cd5f99
Gabriel Souza <rorato91@gmail.com>
2023-04-19 11:41:46 -0300

    aula 36 e algumas mudanças de arquivos

---
+++ CursoPython3/section01/aula36.py
@@ -0,0 +1,49 @@
+"""
+Operadores de atribuiçãp
+= += -= *= /= %= **= //=
+"""
+contador = 10
+
+# while contador < 10:
+#     contador = contador + 1
+#     print(contador)
+
+# print('Acabou')
+
+contador = contador + 1
+print('=', contador)
+
+contador = 10
+
+contador += 1
+print('+=', contador)
+
+contador = 10
+
+contador -= 1
+print('-=', contador)
+
+contador = 10
+
+contador *= 1
+print('*=', contador)
+
+contador = 10
+
+contador /= 1
+print('/=', contador)
+
+contador = 10
+
+contador %= 1
+print('%=', contador)
+
+contador = 10
+
+contador **= 1
+print('**=', contador)
+
+contador = 10
+
+contador //= 1
+print('//=', contador)
