cf825c208056024ffb6bb511539a886a2d990326
Gabriel Souza <rorato91@gmail.com>
2023-04-25 15:31:13 -0300

    Exer 13 até 15
    Arquivos .java e .class dos exer

--- CursoJava/Exer/src/Outros/ListaAlgoritimo/Algoritimo/Exer13.java
+++ CursoJava/Exer/src/Outros/ListaAlgoritimo/Algoritimo/Exer13.java
@@ -1,4 +1,27 @@
+/*
+13. Ler um número inteiro (assuma até três dígitos)
+e imprimir a saída da seguinte forma:
+CENTENA = x
+DEZENA = x
+UNIDADE = x
+ */
+
 package Outros.ListaAlgoritimo.Algoritimo;
 
+import java.util.Scanner;
+
 public class Exer13 {
+    public static void main(String[] args) {
+        int number;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro de até três dígitos: ");
+        number = sc.nextInt();
+
+        System.out.println("CENTENA = " + (number / 100));
+        System.out.println("DEZENA = " + ((number % 100) / 10));
+        System.out.println("UNIDADE = " + (number % 10));
+
+        sc.close();
+    }
 }
---
+++ CursoJava/Exer/src/Outros/ListaAlgoritimo/Algoritimo/Exer15.java
@@ -0,0 +1,33 @@
+/*
+15. Três amigos, Carlos, André e Felipe.
+decidiram rachar igualmente a conta de um bar.
+Faça um algoritmo para ler o valor total da conta
+e imprimir quanto cada um deve pagar,
+mas faça com que Carlos e André não paguem centavos.
+Ex: uma conta de R$101,53 resulta em R$33,00 para Carlos,
+R$33,00 para André e R$35,53 para Felipe.
+ */
+
+package Outros.ListaAlgoritimo.Algoritimo;
+
+import java.util.Scanner;
+
+public class Exer15 {
+    public static void main(String[] args) {
+        double total, carlos, andre, felipe;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor total da conta: ");
+        total = sc.nextDouble();
+
+        carlos = (int) (total / 3);
+        andre = (int) (total / 3);
+        felipe = total - (carlos + andre);
+
+        System.out.println("Carlos deve pagar: " + carlos);
+        System.out.println("André deve pagar: " + andre);
+        System.out.println(String.format("Felipe deve pagar: " + felipe "%.2f");
+
+        sc.close();
+    }
+}
