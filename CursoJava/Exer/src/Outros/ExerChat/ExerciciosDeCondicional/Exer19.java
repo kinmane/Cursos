22063aac4587468b0b5ef963b19553070ef44e73
Gabriel Souza <rorato91@gmail.com>
2023-05-04 09:50:57 -0300

    Exer 19 até 25

--- CursoJava/Exer/src/Outros/ExerChat/ExerciciosDeCondicional/Exer19.java
+++ CursoJava/Exer/src/Outros/ExerChat/ExerciciosDeCondicional/Exer19.java
@@ -9,5 +9,17 @@ import java.util.Scanner;
 public class Exer19 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("O número é positivo.");
+        } else if (num < 0) {
+            System.out.println("O número é negativo.");
+        } else {
+            System.out.println("O número é zero.");
+        }
     }
 }
--- CursoJava/Exer/src/Outros/ExerChat/ExerciciosDeCondicional/Exer20.java
+++ CursoJava/Exer/src/Outros/ExerChat/ExerciciosDeCondicional/Exer20.java
@@ -1,4 +1,24 @@
+/*
+20. Escreva um programa que determine se um número é par e positivo.
+ */
+
 package Outros.ExerChat.ExerciciosDeCondicional;
 
+import java.util.Scanner;
+
 public class Exer20 {
+    public static void main(String[] args) {
+            int num;
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite um número: ");
+            num = sc.nextInt();
+
+            if (num > 0 && num % 2 == 0) {
+                System.out.println("O número é par e positivo.");
+            } else {
+                System.out.println("O número não é par e positivo.");
+            }
+    }
 }
