57c404e85919bf4002871c7f94932eb8ddc5ddad
Gabriel Souza <rorato91@gmail.com>
2023-06-01 14:36:27 -0300

    Inicio Parte 4

---
+++ CursoCsharpUdemy/Exer/IntroducaoOO/Exer02/Funcionario.cs
@@ -0,0 +1,7 @@
+﻿namespace Exer02;
+
+public class Funcionario
+{
+    public string Name;
+    public double Salary;
+}
---
+++ CursoCsharpUdemy/Parte4/ComOO/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿namespace SemOO
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
