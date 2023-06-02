7f79d07d1bf2773f37a741887ddb106509bebfc0
Gabriel Souza <rorato91@gmail.com>
2023-06-02 09:51:51 -0300

    Exer 1, 2 e 3

---
+++ CursoCsharpUdemy/Exer/ExerFixacao/Exer01/Retangulo.cs
@@ -0,0 +1,24 @@
+﻿namespace Exer01
+{
+    public class Retangulo
+    {
+        public double Height;
+        public double Width;
+        
+        public double Area()
+        {
+            return Height * Width;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Height + Width);
+        }
+        
+        public double Diagonal()
+        {
+            return System.Math.Sqrt(Height * Height + Width * Width);
+        }
+    }
+}
+
---
+++ CursoCsharpUdemy/Exer/ExerFixacao/Exer03/Aluno.cs
@@ -0,0 +1,37 @@
+﻿namespace Exer03
+{
+    public class Aluno
+    {
+        public string Name;
+        public double Grade1, Grade2, Grade3;
+        
+        public double FinalGrade()
+        {
+            return Grade1 + Grade2 + Grade3;
+        }
+        
+        public bool Approved()
+        {
+            if (FinalGrade() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public double MissingPoints()
+        {
+            if (Approved())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - FinalGrade();
+            }
+        }
+    }
+}
