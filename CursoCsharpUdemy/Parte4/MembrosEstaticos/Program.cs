1995262392c344d8292498a29d45a8ba9040680a
Gabriel Souza <rorato91@gmail.com>
2023-06-02 18:01:21 -0300

    Commit

---
+++ CursoCsharpUdemy/Parte4/MembrosEstaticos/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MembrosEstaticos
+{
+    public class Program
+    {
+        static double Pi = 3.14;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2"));
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4 / 3 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
