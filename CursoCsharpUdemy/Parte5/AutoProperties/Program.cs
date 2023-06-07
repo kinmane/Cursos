db2b4aa53cd79c3fb1611e01a8216d9742eb53d7
Gabriel Souza <rorato91@gmail.com>
2023-06-07 15:20:22 -0300

    Props

---
+++ CursoCsharpUdemy/Parte5/AutoProperties/Program.cs
@@ -0,0 +1,14 @@
+﻿using AutoProperties;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
