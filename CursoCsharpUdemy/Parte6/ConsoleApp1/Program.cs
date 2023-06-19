0a8367784dfefa075a580576dc9e07ed63bdcb88
Gabriel Souza <rorato91@gmail.com>
2023-06-19 15:45:17 -0300

    struct

--- CursoCsharpUdemy/Parte6/ConsoleApp1/Program.cs
+++ CursoCsharpUdemy/Parte6/ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TipoReferenciaTipoValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1;
+
+            p1.x = 10;
+            p1.y = 20;
+            Console.WriteLine(p1);
+
+            p1 = new Point();
+            Console.WriteLine(p1);
+        }
+    }
+}
