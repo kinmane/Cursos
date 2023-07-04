a08bd0d2d7850c3da012dd34442f80fc7c0eec50
Gabriel Souza <rorato91@gmail.com>
2023-07-04 16:01:25 -0300

    GarbageCollectorEscopoLocal

--- CursoCsharpUdemy/Parte6/GarbageCollectorEscopoLocal/Program.cs
+++ CursoCsharpUdemy/Parte6/GarbageCollectorEscopoLocal/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace GarbageCollectorEscopoLocal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foi apenas Slide");
+            
+            /*
+             * Objetos alocados dinamicamente, quando não possuem mais referências,
+             * serão desalojados pelo Garbage Collector.
+             *
+             * Variáveis locais são desalocadas imediatamente assim que seu escopo
+             * local sai de execução.
+             */
+        }
+    }
+}
