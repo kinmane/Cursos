0c6f97a32327b963e75fa9993fcf4f4e834918db
Gabriel Souza <rorato91@gmail.com>
2023-07-25 15:44:07 -0300

    Update teste.py

--- CursoPython3/teste.py
+++ CursoPython3/teste.py
@@ -1,16 +1,1 @@
-def main():
-    n = int(input())
-
-    vect = []
-
-    for i in range(n):
-        vect.append(float(input()))
-
-    sum = 0
-
-    for i in range(n):
-        sum += vect[i]
-
-    avg = sum / n
-
-    print(f"AVERAGE HEIGHT = {avg:.2f}")
+print('Digite o primeiro número: ')
