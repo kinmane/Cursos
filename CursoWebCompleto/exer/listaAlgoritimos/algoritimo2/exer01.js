603f509bf873fcf8b16aff83a2e328d6d4facbda
Gabriel Souza <rorato91@gmail.com>
2023-07-31 17:36:31 -0300

    Exer 01 até 03

---
+++ CursoWebCompleto/exer/listaAlgoritimos/algoritimo2/exer01.js
@@ -0,0 +1,15 @@
+/*
+1. Faça um algoritmo que leia os valores A, B, C e imprima na tela
+se a soma de A + B é menor que C.
+ */
+
+const a = 10;
+const b = 20;
+const c = 30;
+
+if (a + b < c) {
+    console.log(`${a} + ${b} é menor que ${c}`);
+}
+else {
+    console.log(`${a} + ${b} não é menor que ${c}`);
+}
---
+++ CursoWebCompleto/exer/listaAlgoritimos/algoritimo2/exer03.js
@@ -0,0 +1,12 @@
+/*
+3) Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+ */
+
+const number = 5;
+
+if (number % 2 === 0) {
+    console.log("Par")
+}
+else {
+    console.log("Ímpar")
+}
