bd95b35800bc3a4306292d92bc7f8c9ef066d7e5
Gabriel Souza <rorato91@gmail.com>
2023-08-08 11:52:47 -0300

    Alooo, to ligando so pra ver como vc está

---
+++ CursoCsharpUdemy/Exer/Redimencionador/Redimencionador/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Redimencionador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando redimencionador.");
+        }
+    }
+}
