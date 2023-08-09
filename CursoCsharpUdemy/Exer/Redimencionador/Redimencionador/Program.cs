9b5ceac2eb543df2f80eda1209b0898b12c41376
Gabriel Souza <rorato91@gmail.com>
2023-08-09 17:54:18 -0300

    redimensionar

--- CursoCsharpUdemy/Exer/Redimencionador/Redimencionador/Program.cs
+++ CursoCsharpUdemy/Exer/Redimencionador/Redimencionador/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Redimencionador
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Redimensionador
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Iniciando redimencionador.");
+            Console.WriteLine("Iniciando redimensionador.");
+
+            Thread thread = new Thread(Redimensionar);
+            thread.Start();
+
+
+
+            Console.Read();
+        }
+
+        static void Redimensionar()
+        {
+            #region "Diretorios"
+            string entradas = "ArquivosEntrada";
+            string redimensionados = "ArquivosRedimensionado";
+            string finalizados = "ArquivosFinalizados";
+
+            if (!Directory.Exists(entradas))
+            {
+                Directory.CreateDirectory(entradas);
+            }
+            if (!Directory.Exists(redimensionados))
+            {
+                Directory.CreateDirectory(redimensionados);
+            }
+            if (!Directory.Exists(finalizados))
+            {
+                Directory.CreateDirectory(finalizados);
+            }
+            #endregion
+
+            while (true)
+            {
+                var arquivosEntrada = Directory.EnumerateFiles(entradas);
+
+                Thread.Sleep(new TimeSpan(0, 0, 5));
+            }
         }
     }
 }
