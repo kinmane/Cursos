94b1cb985ea60ae51cbcc195d0a18396329629aa
Gabriel Souza <rorato91@gmail.com>
2023-08-14 17:39:42 -0300

    é

---
+++ CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Model/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesFilmes.Model
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate {get; set;}
+    }
+}
