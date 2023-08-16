0317191cb881c73859073c082592b19c7f6242ab
Gabriel Souza <rorato91@gmail.com>
2023-08-16 17:21:17 -0300

    commit

--- CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Model/Movie.cs
+++ CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Model/Movie.cs
@@ -5,10 +5,17 @@ namespace RazorPagesFilmes.Model
     public class Movie
     {
         public int Id { get; set; }
+
+        [Display(Name = "Título")]
         public string Tittle { get; set; } = string.Empty;
+
+        [Display(Name = "Gênero")]
         public string Genre { get; set; } = string.Empty;
+
+        [Display(Name = "Preço")]
         public decimal Price { get; set; }
 
+        [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
     }
--- CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Pages/Movies/Index.cshtml.cs
+++ CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesFilmes.Data;
 using RazorPagesFilmes.Model;
@@ -21,11 +22,27 @@ namespace RazorPagesFilmes.Pages.Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string TermoBusca { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string FilmeGenero { get; set; }
+        
+        public SelectList Generos { get; set; }
+
         public async Task OnGetAsync()
         {
+            var filmes = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(TermoBusca))
+            {
+                filmes = filmes.Where(f => f.Tittle.Contains(TermoBusca));
+            }
+
             if (_context.Movie != null)
             {
-                Movie = await _context.Movie.ToListAsync();
+                Movie = await filmes.ToListAsync();
             }
         }
     }
