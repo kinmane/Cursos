8ec8d2e0cae96ab19ce0a8852d983ca7d6927f71
Gabriel Souza <rorato91@gmail.com>
2023-08-16 13:57:50 -0300

    Deu certo, bora continuar

---
+++ CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Model/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesFilmes.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesFilmes.Data;
+using RazorPagesFilmes.Model;
+
+namespace RazorPagesFilmes.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesFilmes.Data.RazorPagesFilmesContext _context;
+
+        public IndexModel(RazorPagesFilmes.Data.RazorPagesFilmesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
