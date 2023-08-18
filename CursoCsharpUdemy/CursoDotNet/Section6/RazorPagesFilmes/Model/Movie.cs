76577f5b0188996e509a91e6e2a998b7e32f1654
Gabriel Souza <rorato91@gmail.com>
2023-08-18 17:40:06 -0300

    Projeto MVC
    Iniciado a seção 7 com apresentação do MVC

--- CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Model/Movie.cs
+++ CursoCsharpUdemy/CursoDotNet/Section6/RazorPagesFilmes/Model/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesFilmes.Model
 {
@@ -7,22 +8,26 @@ namespace RazorPagesFilmes.Model
         public int Id { get; set; }
 
         [Display(Name = "Título")]
-        [Required]
+        [Required(ErrorMessage = "Informe o nome do filme.")]
         [StringLength(40, MinimumLength = 2)]
         public string Tittle { get; set; } = string.Empty;
 
         [Display(Name = "Gênero")]
-        [Required]
+        [RegularExpression(@"^[a+zA-Z\s]*$")]
+        [Required(ErrorMessage = "Informe o gênero do filme.")]
         [StringLength(15, MinimumLength = 3)]
         public string Genre { get; set; } = string.Empty;
 
         [Display(Name = "Preço")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
         [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [Range(0,5)]
         public int Rating { get; set; } = 0;
     }
 }
