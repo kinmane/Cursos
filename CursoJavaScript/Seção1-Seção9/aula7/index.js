989fdfda180b4c95278539fbfa1ee389d2a0c576
Gabriel Souza <rorato91@gmail.com>
2024-01-25 16:26:49 -0300

    commit

--- CursoJavaScript/Seção1-Seção9/aula7/index.js
+++ CursoJavaScript/Seção1-Seção9/aula7/index.js
@@ -6,10 +6,14 @@
 // Case-sensitive
 // Não pode modificar o valor de uma constantes
 
-const primeiroNome = 5;
-const segundoNome = 10;
-const resultado = primeiroNome * segundoNome;
-const resultadoDuplicado = resultado * 2;
+// const primeiroNome = 5;
+// const segundoNome = 10;
+// const resultado = primeiroNome * segundoNome;
+// const resultadoDuplicado = resultado * 2;
 
-console.log(resultadoDuplicado)
-console.log(typeof(primeiroNome))
+// console.log(resultadoDuplicado)
+// console.log(typeof(primeiroNome))
+
+const teste = 1 + '1';
+
+console.log(teste)
