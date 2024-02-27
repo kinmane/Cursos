882684f0a8b1ce40d63991ce9f17516552e94cc2
Gabriel Souza <rorato91@gmail.com>
2024-02-27 15:46:12 -0300

    exer 02 e 03

---
+++ CursoJava/Exer/EstruturaSequencial/src/Exer04.java
@@ -0,0 +1,29 @@
+/*
+Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+decimais.
+ */
+
+import java.util.Scanner;
+
+public class Exer04 {
+    public static void main(String[] args) {
+
+         Scanner sc = new Scanner(System.in);
+
+         int numeroFuncionario, horasTrabalhadas;
+         double valorHora, salario;
+
+         System.out.println("Digite o número do funcionário: ");
+         numeroFuncionario = sc.nextInt();
+         System.out.println("Digite o número de horas trabalhadas: ");
+         horasTrabalhadas = sc.nextInt();
+         System.out.println("Digite o valor que recebe por hora: ");
+         valorHora = sc.nextDouble();
+
+         salario = horasTrabalhadas * valorHora;
+         
+         System.out.println("NUMBER = " + numeroFuncionario);
+         System.out.printf("SALARY = U$ %.2f%n", salario);
+    }
+}
