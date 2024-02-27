b2d73c7145cacd77df79ed57279b9b0f17711641
Gabriel Souza <rorato91@gmail.com>
2024-02-27 15:35:28 -0300

    commit

---
+++ CursoJava/Exer/EstruturaSequencial/src/Exer01.java
@@ -0,0 +1,24 @@
+/*
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos
+*/
+
+import java.util.Scanner;
+
+public class Exer01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero01, numero02;
+
+        System.out.println("Digite o primeiro valor inteiro: ");
+        numero01 = sc.nextInt();
+
+        System.out.println("Digite o segundo valor inteiro: ");
+        numero02 = sc.nextInt();
+
+        System.out.println("SOMA = " + (numero01 + numero02));
+
+    }
+}
