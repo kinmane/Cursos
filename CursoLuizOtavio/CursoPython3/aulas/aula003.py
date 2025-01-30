3b175ba16f362fe39c12531b2503e1864474d09f
Gabriel Souza <rorato91@gmail.com>
2025-01-30 11:05:43 -0300

    commit

---
+++ CursoLuizOtavio/CursoPython3/aulas/aula003.py
@@ -0,0 +1,14 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+
+# Aspas duplas
+
+# Escape
+
+# r
